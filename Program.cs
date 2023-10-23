8f5f242c75d555e301b441f4749199f860cc726d
gaurav <ymdt142@gmail.com>
2023-10-23 14:43:16 +0530

    Test

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("give value for a and b");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Result is "+(a+b));
+            Console.ReadKey();
+        }
+    }
+}
